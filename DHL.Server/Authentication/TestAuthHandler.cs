dd0908bfc77119312bffdc010bb157264402824c
Roman Režňák <roman.reznak@seznam.cz>
2025-02-28 15:57:20 +0100

    28.02.2025_BlankNew

---
+++ DHL.Server/Authentication/TestAuthHandler.cs
@@ -0,0 +1,38 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Options;
+using System.Security.Claims;
+using System.Text.Encodings.Web;
+using System.Threading.Tasks;
+
+namespace DHL.Server.Authentication
+{
+    public class TestAuthHandler : AuthenticationHandler<AuthenticationSchemeOptions>
+    {
+        private readonly TimeProvider _timeProvider;
+
+        public TestAuthHandler(
+            IOptionsMonitor<AuthenticationSchemeOptions> options,
+            ILoggerFactory logger,
+            UrlEncoder encoder,
+            TimeProvider timeProvider)
+            : base(options, logger, encoder)
+        {
+            _timeProvider = timeProvider;
+        }
+
+        protected override Task<AuthenticateResult> HandleAuthenticateAsync()
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.Name, "TestUser"),
+                new Claim(ClaimTypes.Role, "Dispecink") // Testovací role
+            };
+            var identity = new ClaimsIdentity(claims, "TestAuth");
+            var principal = new ClaimsPrincipal(identity);
+            var ticket = new AuthenticationTicket(principal, "TestAuth");
+
+            return Task.FromResult(AuthenticateResult.Success(ticket));
+        }
+    }
+}
--- DHL.Server/Program.cs
+++ DHL.Server/Program.cs
@@ -1,12 +1,31 @@
+using DHL.Server.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.IdentityModel.Tokens;
+using Microsoft.OpenApi.Models;
+using Microsoft.AspNetCore.Authentication;
+
+
 var builder = WebApplication.CreateBuilder(args);
+var authScheme = builder.Environment.IsDevelopment() ? "TestAuth" : JwtBearerDefaults.AuthenticationScheme;
 
 // Add services to the container.
 
 builder.Services.AddControllers();
-// Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
+
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+builder.Services.AddControllers();
 
+builder.Services.AddAuthentication(authScheme)
+    .AddJwtBearer(options =>
+    {
+        options.Authority = "https://enterprise-oauth.company.com"; // OAuth 2.0 server v produkci
+        options.Audience = "your-api";
+        options.RequireHttpsMetadata = false;
+    })
+    .AddScheme<AuthenticationSchemeOptions, TestAuthHandler>("TestAuth", null); // Pøidání fake autentizace pro vývoj
+
+builder.Services.AddAuthorization();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
@@ -16,10 +35,12 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
+app.MapFallbackToFile("index.html");
 app.MapControllers();
 
 app.Run();
